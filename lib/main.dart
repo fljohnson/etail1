9776e863aaaf6c3ed0434193284854622062799c
Floyd Johnson <fljohnson3@isp.com>
2019-06-01 10:36:54 -0400

    Solved issue of iOS navigationButtons being only half-drawn:" padding: EdgeInsets.zero," Up next is order gymnastics: does the Order need to be shipped up in progress, or just before the payment?

--- lib/main.dart
+++ lib/main.dart
@@ -283,6 +283,7 @@ class _AppShellState extends State<AppShell> {
                          );
                        },
                        child:Text("Shop"),
+                       padding: EdgeInsets.zero,
 
                      ),
 
@@ -460,6 +461,7 @@ class CartButton extends StatelessWidget {
     });
     if(isIOS()){
       return CupertinoButton(
+          padding: EdgeInsets.zero,
         onPressed: (){},
         child:Row(children:[Icon(CupertinoIcons.shopping_cart),Text("$numItems")])
       );
@@ -582,16 +584,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
